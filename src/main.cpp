5a61dece4be6f1066252320bc173a6b0843a6abe
Jacob Nilsson <jacob@jacobnilsson.com>
2025-05-16 23:20:44 +0200

    Some things

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,31 @@
 #include <gccore.h>
 #include <wiiuse/wpad.h>
 #include <grrlib.h>
+#include <img.jpg.hpp>
+
+template <typename T, std::size_t U>
+class ImageHandler {
+    GRRLIB_texImg* img;
+public:
+    ImageHandler(const std::array<T, U>& data) {
+        img = GRRLIB_LoadTextureJPG(data.data());
+        if (!img) {
+            throw std::runtime_error("Failed to load image");
+        }
+    }
+    void draw() {
+        if (!img) {
+            throw std::runtime_error("Image not loaded");
+        }
+        GRRLIB_DrawImg(10, 50, img, 0, 1, 1, COLOR_WHITE);
+        GRRLIB_Render();
+    }
+    ~ImageHandler() {
+        if (img) {
+            GRRLIB_FreeTexture(img);
+        }
+    }
+};
 
 void init() {
     VIDEO_Init();
@@ -21,10 +46,14 @@ void init() {
     VIDEO_WaitVSync();
 
     if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+    GRRLIB_Init();
 }
 
 int main() {
     init();
+    ImageHandler<std::uint8_t, img_jpg.size()> imageHandler(img_jpg);
+    imageHandler.draw();
 
     std::this_thread::sleep_for(std::chrono::seconds(5));
 }
